97cff9b47aec2de322dc6550b6b44024047a8e6b
Cyril Connan <cyril.connan@gmail.com>
2023-10-17 14:34:56 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
 import os
 from flask import Flask, render_template
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print("")
     return render_template('index.html')
 
 if __name__ == '__main__':
